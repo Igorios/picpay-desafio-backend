0234c2ba632056c2f54627dcf561a863980cbcb9
Igor Borges <borgesigor153@gmail.com>
2024-09-07 18:48:04 -0300

    :sparkles: feat: criando o service da notificacao

--- src/main/java/com/picpay/services/TransactionService.java
+++ src/main/java/com/picpay/services/TransactionService.java
@@ -2,10 +2,15 @@ package com.picpay.services;
 
 import java.math.BigDecimal;
 
+import java.util.Map;
+import java.time.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.picpay.domain.transaction.Transaction;
 import com.picpay.domain.user.User;
 import com.picpay.dtos.TransactionDTO;
 import com.picpay.repositories.TransactionRepository;
@@ -28,6 +33,34 @@ public class TransactionService {
     
         userService.validateTransaction(sender, transaction.value());
 
+        boolean isAuthorized = this.authorizeTrasaction(sender, transaction.value());
+        if (!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transaction.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        this.transactionRepository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+
+    }
+
+    public boolean authorizeTrasaction(User sender, BigDecimal value) {
+        
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+    
+        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("status");
+            return "success".equalsIgnoreCase(message);
+        } else return false;
     }
 
 }
